98b14f8df1ade71bf1a53986f6358a52d7b90418
ziyanjupan <1258811819@qq.com>
2021-01-23 22:21:30 +0800

    http 跳转 https

---
+++ taco-cloud/src/main/java/taco/config/ConnectorConfig.java
@@ -0,0 +1,55 @@
+package taco.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 重定向配置
+ */
+@Configuration
+public class ConnectorConfig {
+
+    @Value("${http.port}")
+    private int serverPortHttp;
+
+    @Value("${server.port}")
+    private int serverPortHttps;
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection securityCollection = new SecurityCollection();
+                securityCollection.addPattern("/*");
+                securityConstraint.addCollection(securityCollection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        factory.addAdditionalTomcatConnectors(redirectConnector());
+        return factory;
+    }
+
+    /**
+     * http 跳转 https
+     * @return
+     */
+    private Connector redirectConnector(){
+        Connector connector = new Connector(Http11NioProtocol.class.getName());
+        connector.setScheme("http");
+        connector.setPort(serverPortHttp);
+        connector.setSecure(false);
+        connector.setRedirectPort(serverPortHttps);
+        return connector;
+    }
+}
